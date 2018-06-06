d863dc4b66035c4cea272d2837827981d86f3f8b
douglasstruble <dastruble@gmail.com>
2018-06-05 22:52:39 -0700

    ben edits

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 
 function toggle_visibility(id) {
 
-    var e;
+    var e, p, i, self;
     e = document.getElementById(id);
-
+    
+    //show or hide the one we requested
     if (e.style.display === "none") {
         e.style.display = "block";
-    } else {
-        e.style.display = "none";
+    } else { e.style.display = 'none'; }
+    
+    
+    //close all currently open projects
+    p = document.getElementsByClassName("reveal");
+    for (i = 0; i < p.length; i++) {
+        if (p[i].id === e.id) { continue; }
+        p[i].style.display = "none";
     }
 }
